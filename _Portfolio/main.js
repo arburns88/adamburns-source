d4fe529d01cfde621c94ba8512dcf4e255e1ce28
Adam <70457623+arburns88@users.noreply.github.com>
2022-08-22 17:19:08 -0700

    Add files via upload

---
+++ _Portfolio/main.js
@@ -0,0 +1,34 @@
+// always starts the page at the top
+window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+};
+
+// carousel
+document.querySelectorAll(".carousel").forEach(carousel => {
+    let items = carousel.querySelectorAll(".carousel-item");
+    let buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel-button"></span>`;
+    });
+    carousel.insertAdjacentHTML("beforeend", `
+        <div class="carousel-nav">
+            ${ buttonsHtml.join("") }
+        </div>
+    `);
+
+    let buttons = carousel.querySelectorAll(".carousel-button");
+
+    buttons.forEach((button, i) => {
+        button.addEventListener("click", () =>{
+            // unselect all items
+            items.forEach(item => item.classList.remove("carousel-item-selected"));
+            buttons.forEach(button => button.classList.remove("carousel-button-selected"));
+
+            items[i].classList.add("carousel-item-selected");
+            button.classList.add("carousel-button-selected");
+        });
+    });
+    // starts page at 1st carousel item on load
+    items[0].classList.add("carousel-item-selected");
+    buttons[0].classList.add("carousel-button-selected");
+});
+
